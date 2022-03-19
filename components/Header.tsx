dfa91228b3e4a96f5fa1666e6037781dc2baa451
Roj <ez@roj.im>
2022-03-20 01:26:51 +0300

    retranslate header

--- components/Header.tsx
+++ components/Header.tsx
@@ -36,7 +36,7 @@ export function Header({
             {!main &&
               (
                 <div class="font-bold text-gray-900 leading-tight text-2xl sm:text-3xl tracking-tight">
-                  Deno
+                  دێنۆ
                 </div>
               )}
             {subtitle &&
@@ -65,7 +65,7 @@ export function Header({
                   />
                   <div class="ml-5 flex flex-col justify-center">
                     <div class="font-bold text-gray-900 leading-tight text-2xl sm:text-3xl tracking-tight">
-                      Deno
+                      دێنۆ
                     </div>
                     {subtitle &&
                       (
@@ -98,7 +98,7 @@ export function Header({
                   href="https://deno.com/deploy"
                   class="block px-3 py-2 rounded-md text-base font-medium rounded-lg border-2 border-gray-700 bg-transparent text-gray-700 hover:border-gray-900 hover:bg-gray-900 hover:text-gray-50 focus:(outline-none text-gray-900 bg-gray-50) transition duration-150 ease-in-out"
                 >
-                  Deploy
+                  دیپڵۆی
                 </a>
                 {entries.map(({ href, content }) => (
                   <a
@@ -138,7 +138,7 @@ export function Header({
             href="https://deno.com/deploy"
             class="font-medium py-2 px-3 rounded-lg border-2 border-gray-700 bg-transparent text-gray-700 hover:border-gray-900 hover:bg-gray-900 hover:text-gray-50 transition duration-150 ease-in-out"
           >
-            Deploy
+            دیپڵۆی
           </a>
           {entries.map(({ href, content }) => (
             <a
@@ -158,7 +158,7 @@ export function Header({
               fill="currentColor"
               viewBox="0 0 24 24"
             >
-              <title>GitHub | Deno</title>
+              <title>گیتهەب | دێنۆ</title>
               <path
                 fillRule="evenodd"
                 d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
